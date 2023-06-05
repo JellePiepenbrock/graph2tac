34ca4adabc61ca8f15b5d080cfa3327f74dec77a
Jelle Piepenbrock <jellepiepenbrock@gmail.com>
2023-06-05 17:02:08 +0200

    temperature is used for custom forward

--- graph2tac/transformer/pserver.py
+++ graph2tac/transformer/pserver.py
@@ -18,6 +18,11 @@ import time
 import argparse
 import pickle
 import GPUtil
+from typing import Optional, Tuple, Union
+import torch
+from transformers.modeling_outputs import CausalLMOutputWithCrossAttentions
+from torch.nn import CrossEntropyLoss
+
 
 parser = argparse.ArgumentParser(description='Transformer Server')
 
@@ -66,6 +71,83 @@ capnp.remove_import_hook()
 graph_api_filename = pkg_resources.resource_filename('graph2tac.loader','clib/graph_api_v15.capnp')
 graph_api_capnp = capnp.load(graph_api_filename)
 
+# Special forward to control temperature directly.
+class CustomGPT2LMHeadModel(GPT2LMHeadModel):
+
+    def forward(
+            self,
+            input_ids: Optional[torch.LongTensor] = None,
+            past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
+            attention_mask: Optional[torch.FloatTensor] = None,
+            token_type_ids: Optional[torch.LongTensor] = None,
+            position_ids: Optional[torch.LongTensor] = None,
+            head_mask: Optional[torch.FloatTensor] = None,
+            inputs_embeds: Optional[torch.FloatTensor] = None,
+            encoder_hidden_states: Optional[torch.Tensor] = None,
+            encoder_attention_mask: Optional[torch.FloatTensor] = None,
+            labels: Optional[torch.LongTensor] = None,
+            use_cache: Optional[bool] = None,
+            output_attentions: Optional[bool] = None,
+            output_hidden_states: Optional[bool] = None,
+            return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, CausalLMOutputWithCrossAttentions]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Labels for language modeling. Note that the labels **are shifted** inside the model, i.e. you can set
+            `labels = input_ids` Indices are selected in `[-100, 0, ..., config.vocab_size]` All labels set to `-100`
+            are ignored (masked), the loss is only computed for labels in `[0, ..., config.vocab_size]`
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        transformer_outputs = self.transformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            encoder_hidden_states=encoder_hidden_states,
+            encoder_attention_mask=encoder_attention_mask,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        hidden_states = transformer_outputs[0]
+
+        # Set device for model parallelism
+        if self.model_parallel:
+            torch.cuda.set_device(self.transformer.first_device)
+            hidden_states = hidden_states.to(self.lm_head.weight.device)
+
+        lm_logits = self.lm_head(hidden_states) / temperature
+
+        loss = None
+        if labels is not None:
+            # move labels to correct device to enable model parallelism
+            labels = labels.to(lm_logits.device)
+            # Shift so that tokens < n predict n
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+
+        if not return_dict:
+            output = (lm_logits,) + transformer_outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return CausalLMOutputWithCrossAttentions(
+            loss=loss,
+            logits=lm_logits,
+            past_key_values=transformer_outputs.past_key_values,
+            hidden_states=transformer_outputs.hidden_states,
+            attentions=transformer_outputs.attentions,
+            cross_attentions=transformer_outputs.cross_attentions,
+        )
+
+
 def load_eval_setup(toksave, model_location):
     """"
     Load a tokenizer and a model from the specified locations.
@@ -88,7 +170,7 @@ def load_eval_setup(toksave, model_location):
                         pad_token_id=tokenizer(["[PAD]"]).input_ids[0][0]
                         )
 
-    model = GPT2LMHeadModel(config=config)
+    model = CustomGPT2LMHeadModel(config=config)
 
     model.load_state_dict(torch.load(model_location, map_location=torch.device('cpu')))
     model.eval()
@@ -140,7 +222,6 @@ def generate(input_proof_state, tokenizer, model):
             output_scores=True,
             return_dict_in_generate=True,
             length_penalty=0,
-            temperature=temperature,
         )
     else:
         beam_output = model.generate(
